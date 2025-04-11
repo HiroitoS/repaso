8fbf4face94e2085adbaa93b8d2011b5ec8ebd43
HiroitoS <hitoito.sanchez@gmail.com>
2025-04-10 22:48:05 -0500

    terminaod el TODO

--- DOM-TODO-INTERMEDIO/main.js
+++ DOM-TODO-INTERMEDIO/main.js
@@ -5,3 +5,25 @@ const tasks = []
 const taskInput = document.querySelector('.task__input')
 const taskList = document.querySelector('.task__list')
 
+taskInput.addEventListener('keypress', function(event){
+
+  if(event.key === 'Enter'){
+    // console.log('Agrando una nueva nota...')
+    const input = event.target
+    const value = input.value
+    
+    const newTask = {
+      title: value,
+      done : false
+    }
+
+    // console.log(newTask)
+
+    tasks.push(newTask)
+
+    // console.log(tasks)
+    taskInput.value = ''
+
+
+  }
+})
