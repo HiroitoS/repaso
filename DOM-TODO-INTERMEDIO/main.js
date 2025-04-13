73377c95fbf2e88479dc0a0a2764952fc2e058f2
HiroitoS <hitoito.sanchez@gmail.com>
2025-04-12 23:56:35 -0500

    terminando TODO medio

--- DOM-TODO-INTERMEDIO/main.js
+++ DOM-TODO-INTERMEDIO/main.js
@@ -1,6 +1,6 @@
 // console.log('Hola javascript')
 
-const tasks = []
+let tasks = []
 
 const taskInput = document.querySelector('.task__input')
 const taskList = document.querySelector('.task__list')
@@ -24,6 +24,57 @@ taskInput.addEventListener('keypress', function(event){
     // console.log(tasks)
     taskInput.value = ''
 
+    renderTasks()
 
   }
 })
+
+function checkTask(event, currentIndex){
+  // console.log('La tarea fue marcada como hecha', currentIndex)
+  const checkboxSelected = event.target
+  const liParent  = checkboxSelected.parentElement
+  liParent.classList.toggle('isChecked')
+
+
+  const taskSelected = tasks[currentIndex]
+  taskSelected.done = !taskSelected.done
+
+  console.log(tasks)
+}
+
+function removeTask(event, currentIndex){
+
+  console.log('Eliminando', currentIndex)
+  // event.target.parentElement.remove()
+
+  const newTasks = tasks.filter((task, index) => index !== currentIndex)
+
+  tasks = newTasks
+  renderTasks()
+
+}
+
+
+function renderTasks(){
+  // console.log('Dibujando tareas...')
+  let list= ''
+  tasks.forEach(function(task, index){
+    // console.log(task, index)
+
+    // list = list + task.title
+
+
+    list = list + `
+    <li>
+       <input type="checkbox"
+       onchange="checkTask(event, ${index})"
+       />
+       <span>${task.title}</span>
+       <button onClick = "removeTask(event,${index}) ">Borrar</button>
+    </li>
+      ` 
+  })
+
+  //  console.log(list)  
+  taskList.innerHTML = list
+  }
