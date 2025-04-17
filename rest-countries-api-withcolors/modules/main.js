c3ac534014dd569ce53a5d9c5873d423eec27ba6
HiroitoS <hitoito.sanchez@gmail.com>
2025-04-16 20:37:34 -0500

    jalando datos de la API

--- rest-countries-api-withcolors/modules/main.js
+++ rest-countries-api-withcolors/modules/main.js
@@ -1,6 +1,6 @@
 // console.log('Hola JS')
 
-const url = 'https://restcountries.com/v3.1/all?fields=name,flags,capital,population,languages,currencies,timezones'
+const url = 'https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,languages,currencies,timezones'
 
 // console.log(url)
 
@@ -19,12 +19,41 @@ const fetchCountries = async () => {
   }catch(error){
     console.log('Error en la peticion', error)
   }
+}
 
+const renderCountries = (countries) => {
+  // console.log(countries)
 
+  const countryListElement = document.querySelector('.app__list')
 
+  let countryList = ''
+
+  countries.forEach(country => {
+    console.log(country)  
+    countryList += `
+      <div class="country">
+      <img src="${country.flags.png}" alt="${country.flags.alt}" class="country__flag">
+      <div class="country__wrapper">
+        <h2 class="country__title">${country.name.common}</h2>
+        <div class="country__description">
+          <strong>Population:</strong>${country.population}
+        </div>
+        <div class="country__description">
+          <strong>Region:</strong>${country.region}
+        </div>
+        <div class="country__description">
+          <strong>Capital:</strong>${country.capital[0]} 
+        </div>
+      </div>
+
+    </div>
+    `
+  })
+
+  countryListElement.innerHTML = countryList
 }
 
 fetchCountries()
-.then(data=> console.log(data))
+.then(data=> renderCountries(data))
 
 // TODO : Reescribir async/await para la funcion 
