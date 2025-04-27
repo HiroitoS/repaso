fe3849096dfadc12b53591e35a6a2d18499dc52d
HiroitoS <hitoito.sanchez@gmail.com>
2025-04-27 15:57:16 -0500

    termianando el TODO

--- semana09/dia03-react-todo-app/src/App.jsx
+++ semana09/dia03-react-todo-app/src/App.jsx
@@ -1,18 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
-  return (
-    <main className='bg-yellow-100 w-full max-w-sm mx-auto mt-10 border-yellow-600 rounded-lg shadow-lg p-4'>
-      <h1 className='text-2xl font-bold'>TODO App</h1>
+  const DEFAULT_TODOS = [ 
+    { 
+      "id": 1, 
+      "title": "delectus aut autem", 
+      "completed": false 
+    }, 
+    { "id": 2, 
+      "title": "quis ut nam facilis et officia qui", 
+      "completed": true 
+    }, 
+    { "id": 3, 
+      "title": "fugiat veniam minus", 
+      "completed": false 
+    }
+  ]
 
-      <form action="">
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+  const [input, setInput] = useState('')
+
+
+  const handleChange = (event) => {
+    // console.log(event.target.value)
+
+    const value = event.target.value
+    setInput(value)
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    // console.log(input)
+    const newTodo = {
+      id: todos.length + 1,
+      title: input,
+      completed: false
+    }
+    setTodos([...todos, newTodo])
+    setInput('')
+  }
+
+  return (
+    <main 
+    className='bg-yellow-100 w-full max-w-sm mx-auto mt-10 border border-yellow-600 rounded-lg shadow-lg p-4'>
+      <h1 className='text-2xl font-bold'>TODO App</h1>
+        {/* {JSON.stringify(todos)} */}
+        {/* {input} */}
+
+      <form action="" onSubmit={handleSubmit}>
         <input 
-        className='w-full border my-3 p-3'
+        className='w-full border my-3 p-3 bg-white'
         type="text"
         placeholder='¿Qué deseas hacer hoy?'
         required
-        value="" />
+        onChange={handleChange}
+        value={input}
+        />
       </form>
+
+      <div className='flex justify-between '>
+        <span className='font-bold '>3 de 8</span>
+        <button 
+        className='bg-blue-500 rounded-b-lg px-2 py-1 text-white hover:bg-blue-800 duration-300'
+        >
+          Limpiar tareas completadas</button>
+      </div>
+
+      <section className='mt-4'>
+        <ul className='flex flex-col gap-2'>
+          {todos.map(todo =>{
+            return(
+              <li className='flex' key={todo.id}>
+                <input type="checkbox" 
+                className='mr-2'
+                />
+                <div className='w-full flex justify-between items-center'> 
+                  <div className=''>
+                    {todo.title}
+                  </div>
+                  <button
+                  className='bg-red-300 rounded-lg px-2 py-2'>
+                    ❌
+                  </button>
+                  </div>
+              </li>              
+            )
+          })}
+        </ul>
+      </section>
       
     </main>
     
